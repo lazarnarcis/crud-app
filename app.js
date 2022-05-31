0dd3f9ddf53360551c19da39f40bfb0c437ffa5f
Lazar Narcis <lnarcis310@gmail.com>
2022-05-31 22:38:24 +0300

    click enter from input to action button

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 let messages = localStorage.getItem("messages") || [];
+let messageText = document.querySelector("#message");
 
 function sendMessage() {
-    let messageText = document.querySelector("#message");
-    let valMessage = document.querySelector("#message").value;
+    let valMessage = messageText.value;
     if (valMessage == "") return;
     messages.push(valMessage);
     localStorage.setItem("messages", JSON.stringify(messages));
     messageText.value = "";
     showMessages();
 }
+
+messageText.addEventListener("keypress", function (event) {
+    if (event.key == "Enter") sendMessage();
+});
 
 if (messages.length) messages = JSON.parse(messages);
 
