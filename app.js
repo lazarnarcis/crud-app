5e5a0433bbe96640cf39006fb3ca78e6afe84959
Lazar Narcis <lnarcis310@gmail.com>
2022-05-31 22:34:44 +0300

    localStorage for storing messages

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-let messages = [];
+let messages = localStorage.getItem("messages") || [];
 
 function sendMessage() {
     let messageText = document.querySelector("#message");
     let valMessage = document.querySelector("#message").value;
+    if (valMessage == "") return;
     messages.push(valMessage);
+    localStorage.setItem("messages", JSON.stringify(messages));
     messageText.value = "";
     showMessages();
 }
+
+if (messages.length) messages = JSON.parse(messages);
 
 function showMessages() {
     let generalDivMessages = document.querySelector("#messages");
@@ -17,3 +21,5 @@ function showMessages() {
         generalDivMessages.appendChild(textMsg);
     }
 }
+
+showMessages();
