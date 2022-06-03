6771c4735d6f83f1de4d693a3d3dc93c30dce8f1
Lazar Narcis <lnarcis310@gmail.com>
2022-06-03 22:09:55 +0300

    add update part from CRUD

--- app.js
+++ app.js
@@ -21,9 +21,26 @@ function showMessages() {
     generalDivMessages.innerText = '';
     for (let i = 0; i < messages.length; i++) {
         let textMsg = document.createElement("p");
-        textMsg.innerText = messages[i];
+        textMsg.innerHTML = messages[i] + " <button class='button" + i + "' onclick='showEditMessage(" + i + ");'>Update</button> <input type='text' placeholder='Edit message....' style='display: none' class='inputEdit" + i + "' /> <input style='display: none' class='buttonEdit" + i + "' type='button' onclick='editMessage(" + i + ")' value='Update Message' />";
         generalDivMessages.appendChild(textMsg);
     }
+}
+
+function showEditMessage (i) {
+    let btn = document.querySelector(".button" + i);
+    btn.style.display = "none";
+    let inputEdit = document.querySelector(".inputEdit" + i);
+    inputEdit.style.display = "inline";
+    let buttonEdit = document.querySelector(".buttonEdit" + i);
+    buttonEdit.style.display = "inline";
+}
+
+function editMessage (i) {
+    let inputEdit = document.querySelector(".inputEdit" + i);
+    let editValue = inputEdit.value;
+    messages[i] = editValue;
+    showMessages();
+    localStorage.setItem("messages", JSON.stringify(messages));
 }
 
 showMessages();
